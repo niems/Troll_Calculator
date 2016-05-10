eb5135bf464d95fa06088454b7f6671db8783fd2
niems <czniemann1990@gmail.com>
2016-05-10 18:35:03 -0500

    Calculator works with switching operators and with the calculations. Just need to set the display view so it's a fixed height

--- app/src/main/java/com/example/niems/calculator/Calculator.java
+++ app/src/main/java/com/example/niems/calculator/Calculator.java
@@ -96,6 +96,12 @@ public class Calculator extends AppCompatActivity {
                 this.s_display_view = this.s_first_num + " " + this.s_current_operator; //current view to display
             }
 
+            else if( this.s_second_num.equals("") ){ //user is switching operator sign
+                this.s_current_operator = b.getText().toString();
+
+                this.s_display_view = this.s_first_num + " " + this.s_current_operator; //current view to display
+            }
+
             //computation to be done since the second number exists
             else{
                 double temp_total = 0; //used to do the computation of the two numbers
@@ -119,7 +125,7 @@ public class Calculator extends AppCompatActivity {
                 }
 
                 this.s_first_num = Double.toString( temp_total ); //stores the computation in the first number
-                this.s_second_num = "#"; //reset
+                this.s_second_num = ""; //reset to the second number being ready for input
                 this.s_current_operator = b.getText().toString(); //sets the new operator
 
                 this.s_display_view = this.s_first_num + " " + this.s_current_operator; //current view to display
@@ -131,120 +137,6 @@ public class Calculator extends AppCompatActivity {
             Toast.makeText( this, "Operator - error in try block", Toast.LENGTH_SHORT ).show();
         }
     }
-
-    /*
-    //when the user presses a number or the decimal point, it appears on the display view
-    public void numberClick( View view ){
-
-
-        try{
-            TextView display_view = (TextView) findViewById( R.id.display_view ); //gets the display view to modify
-            Button b = (Button) findViewById( view.getId() ); //reads in the current user input
-
-            //put check for if there is already a decimal here. If there is, the decimal key has no effect.
-
-            if( this.b_first_execution ) { //program just started
-
-                Toast.makeText(this, "number - first execution", Toast.LENGTH_SHORT).show();
-
-                this.s_current_num = b.getText().toString(); //overwrites the default display value
-                this.d_current_total = Double.parseDouble( this.s_current_num ); //overwrites default total
-                this.b_first_execution = false;
-
-                this.s_display_view = this.s_current_num; //current view to display to the screen
-            }
-
-            else { //program has been running and computations have been preformed
-                Toast.makeText(this, "number - combines string with current number displayed", Toast.LENGTH_SHORT).show();
-
-                this.s_current_num = this.s_current_num + b.getText().toString();
-                //this.d_current_total = Double.parseDouble( this.s_current_num );
-
-                this.s_display_view = Double.toString( this.d_current_total ) + " " + this.s_current_operator + " " + this.s_current_num;
-            }
-
-            display_view.setText( this.s_display_view ); //current view
-            //display_view.setText( Double.toString( this.d_current_total ) ); //current number is displayed on the main display view
-
-
-        }catch(Exception e){
-            Toast.makeText(this, "An error has occurred in numberClick()", Toast.LENGTH_SHORT).show(); //displays error message to user
-        }
-    }
-    */
-
-    /*when the user presses an operator button(+,-,*,/) the current string number is combined
-    with the current total using the previous operator pressed if applicable. Otherwise,
-    the current string number is set to the current total. After this, the new operator
-    pressed overwrites the current operator.
-
-    *this.s_current_num needs to be cleared when an operator is pressed
-    *
-    */
-
-    /*
-    public void operatorClick( View view ){
-        TextView display_view = (TextView) findViewById( R.id.display_view ); //gets the display view to modify
-        Button b = (Button) findViewById( view.getId() ); //gets the id of the operator pressed
-
-
-        try{
-
-            this.s_current_num = ""; //reset
-
-            if( this.b_first_execution ){ //no commands have been executed
-
-                this.s_current_operator = b.getText().toString(); //sets the new operator
-                this.b_first_execution = false;
-                Toast.makeText(this, "operator - first execution", Toast.LENGTH_SHORT).show();
-            }
-
-            //there is no operator and the program has executed before
-            else if( this.s_current_operator.equals("") ){
-                Toast.makeText(this, "operator - new operator", Toast.LENGTH_SHORT).show();
-                this.s_current_operator = b.getText().toString(); //stores the new operator
-            }
-
-            else{
-                //sets new operator after doing computation
-                Toast.makeText(this, "operator - else statement", Toast.LENGTH_SHORT).show();
-
-                switch( this.s_current_operator ){
-                    case "+":
-                        this.d_current_total = this.d_current_total + Double.parseDouble( this.s_current_num );
-                        break;
-
-                    case "-":
-                        this.d_current_total = this.d_current_total - Double.parseDouble( this.s_current_num );
-                        break;
-
-                    case "*":
-                        this.d_current_total = this.d_current_total * Double.parseDouble( this.s_current_num );
-                        break;
-
-                    case "/":
-                        this.d_current_total = this.d_current_total / Double.parseDouble( this.s_current_num );
-                        break;
-                }
-
-                this.s_current_operator = b.getText().toString(); //sets new operator
-
-
-                //this.s_display_view = Double.toString( this.d_current_total ) + " " + this.s_current_operator;
-               // display_view.setText( this.s_display_view );
-
-                //display_view.setText( Double.toString( this.d_current_total ) );
-            }
-
-            this.s_display_view = Double.toString( this.d_current_total ) + " " + this.s_current_operator;
-            display_view.setText( this.s_display_view );
-
-        }catch(Exception e){
-            Toast.makeText(this, "An error occurred in operatorClick()", Toast.LENGTH_SHORT).show(); //displays error message to user
-        }
-    }
-
-    */
 
     //clears the operator and the current total
     public void clearTotal( View view ){
