2af5930dd18daf946a5cfe0021ddddc8bd391e1c
niems <czniemann1990@gmail.com>
2016-05-10 18:23:10 -0500

    rewrote numberClick() and operatorClick(). Doesn't seem to work after the first computation is calculated and then you try to do a computation to it. Ex. 4 + 3 = 7. 7 + error here.

--- app/src/main/java/com/example/niems/calculator/Calculator.java
+++ app/src/main/java/com/example/niems/calculator/Calculator.java
@@ -30,7 +30,7 @@ public class Calculator extends AppCompatActivity {
 
         //new var
         this.s_first_num = Double.toString( this.d_current_total );
-        this.s_second_num = ""; //not used until later
+        this.s_second_num = "#"; //while the value is '#',
         this.s_display_view = this.s_first_num; //display view default
 
         super.onCreate(savedInstanceState);
@@ -40,22 +40,95 @@ public class Calculator extends AppCompatActivity {
         display_view.setText( this.s_display_view ); //current total displayed in view
     }
 
-    public void numberClick( View view ){
+    public void numberClick( View view ){ //if the user presses a number
         try{
             TextView display_view = (TextView) findViewById( R.id.display_view ); //gets the display view to modify
             Button b = (Button) findViewById( view.getId() ); //button the user pressed
 
 
-            if( !this.s_second_num.equals("") ){ //the second number is to be inputted
+            if( !this.s_second_num.equals("#") ){ //the second number is to be inputted
 
+                if( this.s_second_num.equals("0") || this.s_second_num.equals("0.0") ){
+                    this.s_second_num = b.getText().toString(); //user entered. Needs to be overwritten
+                }
+
+                else{
+                    this.s_second_num += b.getText().toString(); //concatenates current button number with the second number
+                }
+
+                this.s_display_view = this.s_first_num + " " + this.s_current_operator + " " + this.s_second_num; //current view to display
             }
 
             else{ //first number to be inputted
 
+                if( this.s_first_num.equals("0") || this.s_first_num.equals("0.0") ){ //default value or user entered. Needs to be overwritten
+                    this.s_first_num = b.getText().toString();
+                }
+
+                else{ //concatenates current button number with the first number
+                    this.s_first_num += b.getText().toString();
+                }
+
+                this.s_display_view = this.s_first_num; //current view to display
             }
+
+            display_view.setText( this.s_display_view ); //sets new display view
 
         }catch(Exception e){
             Toast.makeText(this, "An error has occurred in numberClick()", Toast.LENGTH_SHORT).show(); //displays error to user
+        }
+    }
+
+    public void operatorClick( View view ){
+
+        try{
+            TextView display_view = (TextView) findViewById( R.id.display_view ); //used to modify the display view
+            Button b = (Button) findViewById( view.getId() ); //button user pressed (the operator)
+
+
+            //no computation to do since the second number has not been inputted yet
+            //this also means the first number is done being inputted, so the second
+            //number is ready for input
+            if( this.s_second_num.equals("#") ){
+                this.s_current_operator = b.getText().toString(); //new operator
+                this.s_second_num = ""; //second number is ready for input
+
+                this.s_display_view = this.s_first_num + " " + this.s_current_operator; //current view to display
+            }
+
+            //computation to be done since the second number exists
+            else{
+                double temp_total = 0; //used to do the computation of the two numbers
+
+                switch( this.s_current_operator ){
+                    case "+":
+                        temp_total = Double.parseDouble( this.s_first_num ) + Double.parseDouble( this.s_second_num );
+                        break;
+
+                    case "-":
+                        temp_total = Double.parseDouble( this.s_first_num ) - Double.parseDouble( this.s_second_num );
+                        break;
+
+                    case "*":
+                        temp_total = Double.parseDouble( this.s_first_num ) * Double.parseDouble( this.s_second_num );
+                        break;
+
+                    case "/":
+                        temp_total = Double.parseDouble( this.s_first_num ) / Double.parseDouble( this.s_second_num );
+                        break;
+                }
+
+                this.s_first_num = Double.toString( temp_total ); //stores the computation in the first number
+                this.s_second_num = "#"; //reset
+                this.s_current_operator = b.getText().toString(); //sets the new operator
+
+                this.s_display_view = this.s_first_num + " " + this.s_current_operator; //current view to display
+            }
+
+            display_view.setText( this.s_display_view ); //sets new display view
+
+        }catch( Exception e ){
+            Toast.makeText( this, "Operator - error in try block", Toast.LENGTH_SHORT ).show();
         }
     }
 
@@ -108,6 +181,8 @@ public class Calculator extends AppCompatActivity {
     *this.s_current_num needs to be cleared when an operator is pressed
     *
     */
+
+    /*
     public void operatorClick( View view ){
         TextView display_view = (TextView) findViewById( R.id.display_view ); //gets the display view to modify
         Button b = (Button) findViewById( view.getId() ); //gets the id of the operator pressed
@@ -169,23 +244,22 @@ public class Calculator extends AppCompatActivity {
         }
     }
 
+    */
+
     //clears the operator and the current total
     public void clearTotal( View view ){
         try{
+            TextView display_view = (TextView) findViewById( R.id.display_view ); //display view to modify
 
-            Toast.makeText(this, "cleared display & values", Toast.LENGTH_SHORT).show();
+            Toast.makeText(this, "cleared display & values", Toast.LENGTH_SHORT).show(); //debugging
 
-            TextView display_view = (TextView) findViewById( R.id.display_view );
-
-            //resets all values
+            //reset to default values
+            this.s_first_num = "0.0";
             this.s_current_operator = "";
-            this.d_current_total = 0;
-            this.s_current_num = Double.toString( this.d_current_total );
-            this.b_first_execution = true;
+            this.s_second_num = "#";
 
-            this.s_display_view = this.s_current_num;
-
-            display_view.setText( this.s_display_view );
+            this.s_display_view = this.s_first_num;
+            display_view.setText( this.s_display_view ); //sets new display view
 
         }catch(Exception e){
             Toast.makeText(this, "An error occurred in clearTotal()", Toast.LENGTH_SHORT).show(); //displays error message to user
